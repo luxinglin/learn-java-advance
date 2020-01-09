f2986f26b0ecd2aee9fd2511a54507225ba356c5
luxinglin <651150151@qq.com>
2020-01-09 12:14:24 +0800

    multi-datasource: 新增组件扫描基础包配置，以支持rest全局异常处理

--- database/multi-datasource/src/main/java/cn/com/gary/database/MultiDatasourceApplication.java
+++ database/multi-datasource/src/main/java/cn/com/gary/database/MultiDatasourceApplication.java
@@ -3,6 +3,7 @@ package cn.com.gary.database;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableAsync
 @EnableTransactionManagement(proxyTargetClass = true)
+@ComponentScan(basePackages = {"cn.com.gary.biz", "cn.com.gary.database"})
 @Slf4j
 public class MultiDatasourceApplication {
 
