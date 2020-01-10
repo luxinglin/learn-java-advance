5b02921eb8ad27063d51d5c310c946ec595d8673
luxinglin <651150151@qq.com>
2020-01-10 17:55:34 +0800

    java-root: 代码路径规范化

--- common-pojo/src/main/java/cn/com/gary/model/exception/PropertyAccessException.java
+++
@@ -1,7 +0,0 @@
-package cn.com.gary.model.exception;
-
-public class PropertyAccessException extends RuntimeException {
-    public PropertyAccessException(Throwable cause) {
-        super(cause);
-    }
-}
