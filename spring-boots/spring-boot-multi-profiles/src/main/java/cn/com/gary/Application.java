fd193445083883f49e9f2f79c5e9145df6e7a2c5
Lu, Xinglin <xinglin.lu@chiefclouds.com>
2023-10-24 11:08:25 +0800

    feat: multi profiles
    test multi profiles

---
+++ spring-boots/spring-boot-multi-profiles/src/main/java/cn/com/gary/Application.java
@@ -0,0 +1,20 @@
+package cn.com.gary;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author EDY
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
+
---
+++ spring-boots/spring-boot-multi-profiles/src/main/java/cn/com/gary/web/HelloController.java
@@ -0,0 +1,24 @@
+package cn.com.gary.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/getCurrentEnv")
+    public String getCurrentEnv() {
+        String[] actProfile = env.getActiveProfiles();
+        String curProfile = actProfile[0];
+        return curProfile;
+    }
+}
